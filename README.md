9807c185491cd6e9743a98b7df672c971e52c26d
Mars Xue <marsxue0221@gmail.com>
2019-05-05 22:11:32 +1000

    Add map view

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CCC_Project_2
+
+### Setup
+```
+npm install --save react-map-gl
+```
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactMapGL, {NavigationControl} from 'react-map-gl';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const TOKEN = 'pk.eyJ1IjoibWFyc3h1ZSIsImEiOiJjanZha3IweDIxZGt5NDRzMXlobmhjbGRyIn0.F3PshM4rzEMkJHMh6QqPkw';
+
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: -37.8136,
+        longitude: 144.9631,
+        zoom: 11,
+        bearing: 0,
+        pitch: 0,
+        width: 1440,
+        height: 700,
+      }
+    };
+  }
+  render() {
+    const {viewport} = this.state;
+    return (
+      <div className="App">
+        <center>
+          <h2>COMP90024 Cluster and Cloud Computing Project 2</h2>
+        </center>
+        
+        <ReactMapGL
+          {...viewport}
+          mapStyle="mapbox://styles/mapbox/dark-v9"
+          mapboxApiAccessToken={TOKEN}>
+          <div className="nav" style={navStyle}>
+            <NavigationControl/>
+          </div>
+        </ReactMapGL>
+      </div>
+    );
+  }
 }
 
 export default App;
