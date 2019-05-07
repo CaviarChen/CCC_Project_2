da0c7667f3d1ca9650e08d66ff4e0e72c3e99108
Zijun Chen <c.zijunchen@gmail.com>
2019-05-07 22:12:45 +1000

    Added curl_command_template for add_import_job

--- common_script/config.py
+++ common_script/config.py
@@ -8,6 +8,13 @@ couchdb_admin_party = True
 # tweet_geo_limit = None
 tweet_geo_limit = [144.3336, 145.8784, -38.5030, -37.1751]
 
+curl_command_template = """
+curl "http://127.0.0.1/couchdbro/twitter/_design/twitter/_view/summary" -G \
+--data-urlencode 'start_key=["melbourne",{}]' \
+--data-urlencode 'end_key=["melbourne",{}]' \
+--data-urlencode 'reduce=false'  --data-urlencode 'include_docs=true'  --user "readonly:fakepassword"  -o ./tmp/twitter.json
+"""
+
 # node_id = os.environ["H_NODE_ID"]
 # couchdb_host = os.environ["H_DB_HOST"]
 # couchdb_user = os.environ["H_DB_USER"]
@@ -19,8 +26,8 @@ tweet_geo_limit = [144.3336, 145.8784, -38.5030, -37.1751]
 #     if len(tmp) == 4:
 #         tweet_geo_limit = tmp
 
-print("node_id", node_id)
-print("couchdb_host", couchdb_host)
-print("couchdb_user", couchdb_user)
-print("couchdb_admin_party", couchdb_admin_party)
-print("tweet_geo_limit", tweet_geo_limit)
+# print("node_id", node_id)
+# print("couchdb_host", couchdb_host)
+# print("couchdb_user", couchdb_user)
+# print("couchdb_admin_party", couchdb_admin_party)
+# print("tweet_geo_limit", tweet_geo_limit)
