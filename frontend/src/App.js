2d7556f43f22b48a32cc30e7c59a7bf173ce1510
Mars Xue <marsxue0221@gmail.com>
2019-05-07 16:40:45 +1000

    Update geojson, map view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,78 @@
-import React, { Component } from 'react';
-import ReactMapGL, {NavigationControl} from 'react-map-gl';
-import './App.css';
+import React, { Component } from 'react'
+import mapboxgl from 'mapbox-gl'
 
+import TOKEN from './config.js'
+import * as geoData from './melbourne.geojson'
 
-const TOKEN = 'pk.eyJ1IjoibWFyc3h1ZSIsImEiOiJjanZha3IweDIxZGt5NDRzMXlobmhjbGRyIn0.F3PshM4rzEMkJHMh6QqPkw';
-
-const navStyle = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  padding: '10px'
-};
+mapboxgl.accessToken = TOKEN;
 
 class App extends Component {
-  constructor(props) {
+
+  constructor(props: Props) {
     super(props);
     this.state = {
-      viewport: {
-        latitude: -37.8136,
-        longitude: 144.9631,
-        zoom: 11,
-        bearing: 0,
-        pitch: 0,
-        width: 1440,
-        height: 700,
-      }
+      lng: 144.9631,
+      lat: -37.8136,
+      zoom: 12
     };
   }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/shijiel2/cjvcb640p3oag1gjufck6jcio',
+      center: [lng, lat],
+      zoom
+    });
+
+    map.on('load', function () {
+      map.addLayer({
+        'id': 'suburb-layer',
+        'type': 'fill',
+        'source': {
+          'type': 'geojson',
+          'data': geoData
+        },
+        'paint': {
+          'fill-color': 'rgba(200, 100, 240, 0.4)',
+          'fill-outline-color': 'rgba(200, 100, 240, 1)'
+        }
+      });
+
+      map.addLayer({
+        'id': 'suburb-borders',
+        'type': 'line',
+        'source': {
+          'type': 'geojson',
+          'data': geoData
+        },
+        "paint": {
+          "line-color": "#627BC1",
+          "line-width": 2
+        }
+      });
+    })
+
+    map.on('click', 'suburb-layer', function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(e.features[0].properties.SA2_NAME16)
+        .addTo(map);
+    });
+
+  }
+
   render() {
-    const {viewport} = this.state;
+    const { lng, lat, zoom } = this.state;
     return (
-      <div className="App">
-        <center>
-          <h2>COMP90024 Cluster and Cloud Computing Project 2</h2>
-        </center>
-        
-        <ReactMapGL
-          {...viewport}
-          mapStyle="mapbox://styles/mapbox/dark-v9"
-          mapboxApiAccessToken={TOKEN}>
-          <div className="nav" style={navStyle}>
-            <NavigationControl/>
-          </div>
-        </ReactMapGL>
-      </div>
+        <div>
+          <center>
+            <h2>COMP90024 Cluster and Cloud Computing Project 2</h2>
+          </center>
+          <div style = {{height:"100vh"}} ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+        </div>
     );
   }
 }
