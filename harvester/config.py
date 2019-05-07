747ea43821ad3f90ee9bd7107225a15af9b64998
Zijun Chen <c.zijunchen@gmail.com>
2019-05-07 14:16:31 +1000

    Update.

--- harvester/config.py
+++ harvester/config.py
@@ -5,4 +5,5 @@ couchdb_host = "http://127.0.0.1:5984"
 couchdb_user = None
 couchdb_auth_token = None
 couchdb_admin_party = True
-tweet_remove_no_geo = True
+# tweet_geo_limit = None
+tweet_geo_limit = [144.3336, 145.8784, -38.5030, -37.1751]
