d48aa9e36f80faa02d73ec0a1318705c78afebc7
PwzXxm <cepheidfov@gmail.com>
2019-05-10 21:32:50 +1000

    addd docker build script

---
+++ common_script/build_docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function harvester {
+    docker build -t registry.gitlab.com/cepheidfov/ccc_project_2/harvester:latest ../harvester/
+    docker push registry.gitlab.com/cepheidfov/ccc_project_2/harvester:latest
+}
+
+function importer {
+    docker build -t registry.gitlab.com/cepheidfov/ccc_project_2/importer:latest ../importer
+    docker push registry.gitlab.com/cepheidfov/ccc_project_2/importer:latest
+}
+
+function pre {
+    docker build -t registry.gitlab.com/cepheidfov/ccc_project_2/preprocessor:latest ../preprocessor
+    docker push registry.gitlab.com/cepheidfov/ccc_project_2/preprocessor:latest
+}
+
+printf "1) harvester\n2) importer\n3) pre-processor\ndefault) ALL\n"
+read inpt
+
+case $input in 
+    "1")
+        harvester
+        ;;
+    "2")
+        importer
+        ;;
+    "3")
+        pre
+        ;;
+    "")
+        harvester
+        importer
+        pre
+        ;;
+esac
