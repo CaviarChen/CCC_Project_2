8ffb375104e40d8d2b3ae60e4e99bce7bd4a3725
Mars Xue <marsxue0221@gmail.com>
2019-05-10 17:11:35 +1000

    Update project page

--- frontend/src/Project.js
+++ frontend/src/Project.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Card, Icon, Avatar, Collapse, Typography, Divider } from 'antd';
+import { Row, Col, Card, Icon, Collapse, Typography, Divider } from 'antd';
 
 import AppLayout from './layouts/AppLayout';
 
@@ -65,61 +65,61 @@ class Project extends Component {
             <Row type="flex" justify="space-around" align='middle'>
               <Col span={4}>
                 <Card
-                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                  cover={<img alt="example" src="https://github.com/cmjhaha886.png?size=400" />}
                   actions={[<a href="https://github.com/cmjhaha886" target="_blank" rel="noopener noreferrer"><Icon type="github" style={{ fontSize: '24px' }}/></a>]}
                 >
                   <Meta
-                    avatar={<Avatar src="https://github.com/cmjhaha886.png?size=200" />}
                     title="Minjian Chen"
                     description="Backend"
+                    style={{textAlign: "center"}}
                   />
                 </Card>
               </Col>
               <Col span={4}>
                 <Card
-                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                  cover={<img alt="example" src="https://github.com/shijiel2.png?size=400" />}
                   actions={[<a href="https://github.com/shijiel2" target="_blank" rel="noopener noreferrer"><Icon type="github" style={{ fontSize: '24px' }}/></a>]}
                 >
                   <Meta
-                    avatar={<Avatar src="https://github.com/shijiel2.png?size=200" />}
                     title="Shijie Liu"
                     description="Frontend"
+                    style={{textAlign: "center"}}
                   />
                 </Card>
               </Col>
               <Col span={4}>
                 <Card
-                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                  cover={<img alt="example" src="https://github.com/PwzXxm.png?size=400" />}
                   actions={[<a href="https://github.com/PwzXxm" target="_blank" rel="noopener noreferrer"><Icon type="github" style={{ fontSize: '24px' }}/></a>]}
                 >
                   <Meta
-                    avatar={<Avatar src="https://github.com/PwzXxm.png?size=200" />}
                     title="Weizhi Xu"
                     description="Backend"
+                    style={{textAlign: "center"}}
                   />
                 </Card>
               </Col>
               <Col span={4}>
                 <Card
-                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                  cover={<img alt="example" src="https:github.com/MarsXue.png?size=400" />}
                   actions={[<a href="https://github.com/MarsXue" target="_blank" rel="noopener noreferrer"><Icon type="github" style={{ fontSize: '24px' }}/></a>]}
                 >
                   <Meta
-                    avatar={<Avatar src="https:github.com/MarsXue.png?size=200" />}
                     title="Wenqing Xue"
                     description="Frontend"
+                    style={{textAlign: "center"}}
                   />
                 </Card>
               </Col>
               <Col span={4}>
                 <Card
-                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                  cover={<img alt="example" src="https://github.com/CaviarChen.png?size=400" />}
                   actions={[<a href="https://github.com/CaviarChen" target="_blank" rel="noopener noreferrer"><Icon type="github" style={{ fontSize: '24px' }}/></a>]}
                 >
                   <Meta
-                    avatar={<Avatar src="https://github.com/CaviarChen.png?size=200" />}
                     title="Zijun Chen"
                     description="Backend"
+                    style={{textAlign: "center"}}
                   />
                 </Card>
               </Col>
