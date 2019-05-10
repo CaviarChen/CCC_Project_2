36851cedb8a5bcea98ee5ffbeddc2bc8a7a55851
PwzXxm <cepheidfov@gmail.com>
2019-05-10 13:26:15 +1000

    minor fixes

--- automation/Playbooks/run_db_config.sh
+++ automation/Playbooks/run_db_config.sh
@@ -3,4 +3,4 @@
 if [[ -z "${OS_PROJECT_ID}" ]]; then
   . ../openrc.sh;
 fi
-ansible-playbook -i ../openstack_inventory.py db_config.yaml;
+ansible-playbook --ask-vault-pass -i ../openstack_inventory.py db_config.yaml;
