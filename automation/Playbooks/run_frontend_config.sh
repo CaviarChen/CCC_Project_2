9da49735157c2627b23102ad4fc5aee4dfd3d8f6
PwzXxm <cepheidfov@gmail.com>
2019-05-13 12:22:19 +1000

    add ports for go backend docker && increase size of heap when building frontend

--- automation/Playbooks/run_frontend_config.sh
+++ automation/Playbooks/run_frontend_config.sh
@@ -4,4 +4,4 @@ if [[ -z "${OS_PROJECT_ID}" ]]; then
   . ../openrc.sh;
 fi
 
-ansible-playbook -i ../openstack_inventory.py frontend_config.yaml
+ansible-playbook --ask-vault-pass -i ../openstack_inventory.py frontend_config.yaml
