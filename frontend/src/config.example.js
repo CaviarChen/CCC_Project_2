1414ffcc38a31bca13196715d22d741e949e8507
Zijun Chen <c.zijunchen@gmail.com>
2019-05-13 21:31:40 +1000

    allow user visit from outside.

--- frontend/src/config.example.js
+++ frontend/src/config.example.js
@@ -1,6 +1,12 @@
 // Input your token here
-const TOKEN = '';
-const DATABASE_URL = ''
-const GOBACKEND_URL = ''
+let TOKEN = '';
+let DATABASE_URL = ''
+let GOBACKEND_URL = ''
+
+// change to forwarder if the user is visiting this website outside the school
+if (window.location.hostname === "outside.ccc.8bits.io") {
+    DATABASE_URL.replace('www.ccc.8bits.io', 'outside.ccc.8bits.io')
+    GOBACKEND_URL.replace('www.ccc.8bits.io', 'outside.ccc.8bits.io')
+}
 
 export {TOKEN, DATABASE_URL, GOBACKEND_URL};
